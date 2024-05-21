958364db339e3cdbdee9e486926f527302b3669f
sayhellostasy <stasikvhs2@gmail.com>
2024-05-21 17:17:59 +0300

    28

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/scan', upload.single('card'), function(req, res, next) {
         // uploadFile(reqM);
         // console.log("EVENT : " + JSON.stringify(req.file));
         //console.log("Code change test")
-        console.log('try_harder')
+        console.log('try_harder228')
         res.status(200).json({
             upload : true,
             success : true,
